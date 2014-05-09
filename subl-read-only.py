8d67cfc1eed331536ce173d4afe2f4bdfa23b3c5
David Lutz <del82@cornell.edu>
2014-05-09 13:06:31 -0400

    try to force read-only indicator to be first in status bar

--- subl-read-only.py
+++ subl-read-only.py
@@ -1,13 +1,12 @@
 import sublime, sublime_plugin
 
 class ToggleReadOnlyCommand(sublime_plugin.TextCommand):
-
+    status_name = "1-read-only" # try to make key alphabetically first
     def run(self, edit):
         if self.view.is_read_only():
             self.view.set_read_only(False)
-            self.view.erase_status("read-only")
-
+            self.view.erase_status(self.status_name)
         else:
             self.view.set_read_only(True)
-            self.view.set_status("read-only", "%%")
-            
+            self.view.set_status(self.status_name, "%%")
+
